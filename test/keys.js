35469e3a2c5e6b446044340b1a0a75bc581d225e
Ademar Arvati Filho <aarvati@hotmail.com>
2019-06-11 09:25:39 -0300

    dynamically created test suites
    Signed-off-by: Ademar Arvati Filho <aarvati@hotmail.com>

--- test/keys.js
+++ test/keys.js
@@ -3,22 +3,9 @@ const crypto = require('crypto');
 const cryptoKeys = require('../lib')
 
 describe('Test creation of Private and Public Keys', () => {
-
     var self_ = this;
-    before('Declare function to create KeyPair with node.js crypto', () => {
-        self_.getKeyPair = (
-            type = "ec", 
-            options = {modulusLength: 4096, namedCurve: 'secp256k1', publicKeyEncoding: {type: 'spki', format: 'pem'}, privateKeyEncoding: {type: 'pkcs8', format: 'pem'}}
-            ) => {
-            return {publicKey, privateKey} = crypto.generateKeyPairSync(type, options)
-        }
-    })
-
-    describe('Creation of EC Keys', () => {
-
-        it('EC Key Pair generation using node crypto', () => {
-            // This will only work with higher versions of nodejs >=10
-            const options = {
+    var types = new Map ([
+        ['ec',{
                 modulusLength: 4096, 
                 namedCurve: 'secp256k1', 
                 publicKeyEncoding: {
@@ -32,48 +19,8 @@ describe('Test creation of Private and Public Keys', () => {
                     // passphrase: 'top secret'
                 }
             }
-            const {publicKey, privateKey} = self_.getKeyPair('ec', options)
-            self_.publicKey = publicKey
-            self_.privateKey = privateKey
-            assert.isString(publicKey,'public key is not a string');
-            assert.isString(privateKey,'private key is not a string');
-        })
-
-        it('Importing EC Private Key', () => {
-            self_.cryptoPrivateKey = new cryptoKeys('pem', self_.privateKey);
-            assert.instanceOf(self_.cryptoPrivateKey, cryptoKeys, 'private key is not a cryptoKeys object');
-        })
-
-        it('Exporting EC Private Key', () => {
-            PrivateKey = self_.cryptoPrivateKey.der
-            assert.instanceOf(PrivateKey, Uint8Array, 'private key is not a Uint8Array')
-        })
-
-        it('Importing EC Public Key', () => {
-            self_.cryptoPublicKey = new cryptoKeys('pem', self_.publicKey);
-            assert.instanceOf(self_.cryptoPublicKey, cryptoKeys, 'public key is not a cryptoKeys object');
-        })
-
-        it('Exporting EC Public Key', () => {
-            PublicKey = self_.cryptoPublicKey.der
-            assert.instanceOf(PublicKey, Uint8Array, 'public key is not a Uint8Array')
-        })
-
-        it('Generating EC key public from private key', () => {
-            cryptoPublicKey = cryptoKeys.getPublicKey(self_.cryptoPrivateKey)
-            assert.deepEqual(cryptoPublicKey,self_.cryptoPublicKey,'public key generated from private key is equal original public key');
-            assert.instanceOf(cryptoPublicKey, cryptoKeys, 'public key is not a cryptoKeys object');
-            assert.isFalse(cryptoPublicKey.isPrivate, 'publicKey must not be private')
-            assert.equal((cryptoPublicKey.pem).replace(/\n$/, ""),self_.publicKey .replace(/\n$/, ""), "public pem key is equal original public pem")
-        })
-    });
-
-
-    describe('Creation of RSA Keys', () => {
-
-        it('RSA Key Pair generation using node crypto', () => {
-            // This will only work with higher versions of nodejs >=10
-            const options = {
+        ],
+        ['rsa',{
                 modulusLength: 1024,
                 publicExponent:65537,
                 publicKeyEncoding: {
@@ -87,39 +34,56 @@ describe('Test creation of Private and Public Keys', () => {
                 // passphrase: 'top secret'
                 }
             }
-            const {publicKey, privateKey} = self_.getKeyPair('rsa', options)
-            self_.publicKey = publicKey
-            self_.privateKey = privateKey
-            assert.isString(publicKey,'public key is not a string');
-            assert.isString(privateKey,'private key is not a string');
-        })
+        ]
+    ]);
+    before('Declare function to create KeyPair with node.js crypto', () => {
+        // This will only work with higher versions of nodejs >=10
+        self_.getKeyPair = (
+            type = "ec", 
+            options = {modulusLength: 4096, namedCurve: 'secp256k1', publicKeyEncoding: {type: 'spki', format: 'pem'}, privateKeyEncoding: {type: 'pkcs8', format: 'pem'}}
+            ) => {
+            return {publicKey, privateKey} = crypto.generateKeyPairSync(type, options)
+        }
+    })
 
-        it('Importing RSA Private Key', () => {
-            self_.cryptoPrivateKey = new cryptoKeys('pem', self_.privateKey);
-            assert.instanceOf(self_.cryptoPrivateKey, cryptoKeys, 'private key is not a cryptoKeys object');
-        })
+    types.forEach((value, name) => {
+            describe('Creation of ' + name + ' Keys', () => {
+                it('EC Key Pair generation using node crypto', () => {
+                    const {publicKey, privateKey} = self_.getKeyPair(name, value)
+                    self_.publicKey = publicKey
+                    self_.privateKey = privateKey
+                    assert.isString(publicKey,'public key is not a string');
+                    assert.isString(privateKey,'private key is not a string');
+                })
 
-        it('Exporting RSA Private Key', () => {
-            PrivateKey = self_.cryptoPrivateKey.der
-            assert.instanceOf(PrivateKey, Uint8Array, 'private key is not a Uint8Array')
-        })
+                it('Importing ' + name + ' Private Key', () => {
+                    self_.cryptoPrivateKey = new cryptoKeys('pem', self_.privateKey);
+                    assert.instanceOf(self_.cryptoPrivateKey, cryptoKeys, 'private key is not a cryptoKeys object');
+                })
 
-        it('Importing RSA Public Key', () => {
-            self_.cryptoPublicKey = new cryptoKeys('pem', self_.publicKey);
-            assert.instanceOf(self_.cryptoPublicKey, cryptoKeys, 'public key is not a cryptoKeys object');
-        })
+                it('Exporting ' + name + ' Private Key', () => {
+                    PrivateKey = self_.cryptoPrivateKey.der
+                    assert.instanceOf(PrivateKey, Uint8Array, 'private key is not a Uint8Array')
+                })
 
-        it('Exporting RSA Public Key', () => {
-            PublicKey = self_.cryptoPublicKey.der
-            assert.instanceOf(PublicKey, Uint8Array, 'public key is not a Uint8Array')
-        })
+                it('Importing ' + name + ' Public Key', () => {
+                    self_.cryptoPublicKey = new cryptoKeys('pem', self_.publicKey);
+                    assert.instanceOf(self_.cryptoPublicKey, cryptoKeys, 'public key is not a cryptoKeys object');
+                })
 
-        it('Generating RSA key pair ...', () => {
-            cryptoPublicKey = cryptoKeys.getPublicKey(self_.cryptoPrivateKey)
-            assert.deepEqual(cryptoPublicKey,self_.cryptoPublicKey,'public key generated from private key is equal original public key');
-            assert.instanceOf(cryptoPublicKey, cryptoKeys, 'public key is not a cryptoKeys object');
-            assert.isFalse(cryptoPublicKey.isPrivate, 'publicKey must not be private')
-            assert.equal((cryptoPublicKey.pem).replace(/\n$/, ""),self_.publicKey .replace(/\n$/, ""), "public pem key is equal original public pem")
-        })
-    });
+                it('Exporting ' + name + ' Public Key', () => {
+                    PublicKey = self_.cryptoPublicKey.der
+                    assert.instanceOf(PublicKey, Uint8Array, 'public key is not a Uint8Array')
+                })
+
+                it('Generating ' + name + ' key public from private key', () => {
+                    cryptoPublicKey = cryptoKeys.getPublicKey(self_.cryptoPrivateKey)
+                    assert.deepEqual(cryptoPublicKey,self_.cryptoPublicKey,'public key generated from private key is equal original public key');
+                    assert.instanceOf(cryptoPublicKey, cryptoKeys, 'public key is not a cryptoKeys object');
+                    assert.isFalse(cryptoPublicKey.isPrivate, 'publicKey must not be private')
+                    assert.equal((cryptoPublicKey.pem).replace(/\n$/, ""),self_.publicKey .replace(/\n$/, ""), "public pem key is equal original public pem")
+                })
+            });
+        }
+    )
 },true);
